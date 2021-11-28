b96bf1b4a737a24eee71b6208b6bfeda303a54eb
* <itssng7@gmail.com>
2021-11-28 05:02:27 -0500

    added more shit to the box

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1004,11 +1004,11 @@ class PlayState extends MusicBeatState
       robloxtxt.text = "drippy";
       robloxtxt.setFormat(Paths.font("rblx.ttf"), 30);
       robloxtxt.scrollFactor.set(0.9, 0.9);
-      robloxtxt.y = dad.y + 80;
+      robloxtxt.y = dad.y + 60;
       robloxtxt.x = dad.x;
       
       add(robloxtxt);
-			  dad.y += 250;
+			  dad.y += 150;
 		}
 
 
