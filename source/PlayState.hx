1ae2d011a3082b3fe583f474b2d208c5fbeeddaa
* <itssng7@gmail.com>
2021-11-28 19:47:53 -0500

    aaaa

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1097,7 +1097,7 @@ class PlayState extends MusicBeatState
       robloxtxt.text = "drippy";
       robloxtxt.setFormat(Paths.font("rblx.ttf"), 30);
       robloxtxt.scrollFactor.set(1, 1);
-      robloxtxt.y = dad.y + 90;
+      robloxtxt.y = dad.y + 40;
       robloxtxt.x = dad.x + -20;
       add(robloxtxt);
 		}
